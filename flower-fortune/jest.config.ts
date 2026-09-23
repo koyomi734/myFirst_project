372ac57ff834bf1c8df2cb3383d07571ecc04e5e
HisakoKawaguchi <mikan528@gmail.com>
2026-09-23 21:08:36 +0900

    fix jest.config.ts

--- flower-fortune/jest.config.ts
+++ flower-fortune/jest.config.ts
@@ -2,6 +2,12 @@ import type {Config} from 'jest';
 
 const config: Config = {
   preset: "ts-jest",
+  transform: {
+    "^.+.tsx?$": ["ts-jest", {
+      tsconfig: "tsconfig.jest.json"
+    }],
+  },
+  setupFilesAfterEnv: ['./jest.setup.ts'],
   testEnvironment: "jsdom"
 };
 
